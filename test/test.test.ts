5504be261a2c957791ae764a5f08d4770ff913c0
kyzooghost <jeffersonzjtang@gmail.com>
2022-12-01 21:19:44 +1100

    added unit tests

---
+++ test/test.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('test', async () => {
+  describe('#test()', async () => {
+    it('', async () => {
+      console.log('a');
+      return;
+    }).timeout(10000);
+  });
+});
